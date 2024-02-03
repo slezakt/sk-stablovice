5ed9e74b0af0da82ad7aa6a1f23b1fb4b5a8beae
Tomáš Slezák <info@tomasslezak.cz>
2024-02-03 17:47:56 +0100

    files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import icon from "astro-icon";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  compressHTML: false,
+  build: {
+    inlineStylesheets: 'never',
+    assets: 'assets',
+    compressHTML: false
+  },
+  integrations: [icon()]
+});
---
+++ src/data/header.ts
@@ -0,0 +1,64 @@
+export const menu = [
+    {
+        title: "Home",
+        url: "/"
+    },
+    {
+        title: "O klubu",
+        url: "/o-klubu",
+        submenu: [
+            {
+                title: "Novinky",
+                url: "#"
+            },
+            {
+                title: "Články",
+                url: "#",
+                submenu2: [
+                    {
+                        title: "Historie klubu",
+                        url: "#"
+                    },
+                    {
+                        title: "Reportáže",
+                        url: "#"
+                    },
+                    {
+                        title: "Náš blog",
+                        url: "#"
+                    },
+                ]
+            },
+        ]
+    },
+    {
+        title: "Služby",
+        url: "/sluzby"
+    },
+    {
+        title: "Partneři",
+        url: "/partneri"
+    },
+    {
+        title: "Kontakt",
+        url: "/kontakt"
+    },
+];
+
+export const socials = [
+    {
+        title: "Facebook",
+        url: "facebook.com",
+        icon: "facebook"
+    },
+    {
+        title: "Instagram",
+        url: "instagram.com",
+        icon: "instagram"
+    },
+    {
+        title: "YouTube",
+        url: "youtube.com",
+        icon: "youtube"
+    }
+];
